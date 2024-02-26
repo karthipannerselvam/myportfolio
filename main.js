2f3ae6ca27c1c2f36a7da390d1c22dc103d7a06a
karthipannerselvam <118588657+karthipannerselvam@users.noreply.github.com>
2024-02-26 13:22:31 +0530

    faq added

--- main.js
+++ main.js
@@ -26,3 +26,19 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+
+
+const faqs = document.querySelectorAll(".faq");
+
+faqs.forEach((faq) => {
+    faq.addEventListener('click', () => {
+        faq.classList.toggle("active");
+        const answer = faq.querySelector('.answer');
+        if (faq.classList.contains('active')) {
+            answer.style.maxHeight = answer.scrollHeight + "px";
+        } else {
+            answer.style.maxHeight = null;
+        }
+    });
+});

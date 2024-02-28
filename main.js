7a7e88ba72ed885f9ae2340f654790884dab0fdb
karthipannerselvam <118588657+karthipannerselvam@users.noreply.github.com>
2024-02-28 20:38:47 +0530

    added

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-// const observer =new IntersectionObserver((entries)=>{
-//     entries.forEach((entry)=>{
-//         console.log(entry)
-//         if(entry.isIntersecting){
-//             entry.target.classList.add('show');
-//         }
-//         else{
-//             entry.target.classList.remove('show');
-//         }
-//     });
-// });
-
-
-// const hiddenElements =document.querySelectorAll('.hidden');
-// hiddenElements.forEach((el)=>observer.observe(el));
 
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
@@ -29,16 +14,20 @@ hiddenElements.forEach((el) => observer.observe(el));
 
 
 
-const faqs = document.querySelectorAll(".faq");
+document.addEventListener('DOMContentLoaded', function () {
+    const faqs = document.querySelectorAll(".faq");
 
-faqs.forEach((faq) => {
-    faq.addEventListener('click', () => {
-        faq.classList.toggle("active");
-        const answer = faq.querySelector('.answer');
-        if (faq.classList.contains('active')) {
-            answer.style.maxHeight = answer.scrollHeight + "px";
-        } else {
-            answer.style.maxHeight = null;
-        }
+    faqs.forEach((faq) => {
+        faq.addEventListener('click', () => {
+            faq.classList.toggle("active");
+            const answer = faq.nextElementSibling; // Assuming .answer is a sibling of .faq
+            if (faq.classList.contains('active')) {
+                answer.style.maxHeight = answer.scrollHeight + "px";
+            } else {
+                answer.style.maxHeight = null;
+            }
+        });
     });
 });
+
+
